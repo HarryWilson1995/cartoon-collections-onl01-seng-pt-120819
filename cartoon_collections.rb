3ce69008667fc26299b451dd963056b5ebcc9dcb
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 16:11:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,14 +19,10 @@ def long_planeteer_calls(array)
   
 end
 
-# def find_the_cheese(array) 
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   array.find do |type|
-#     cheese_types.include?(type)
-#   end 
-# end
-def find_the_cheese(array)
-  array.include?("cheddar")
-  array.include?("gouda")
-  array.include?("camembert")
-end 
+def find_the_cheese(array) 
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
